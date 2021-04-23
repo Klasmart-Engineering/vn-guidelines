02ee8ce4ef2befb764db3fad8f6326b35fa83a6a
Dat <doanthanhdat2509@gmail.com>
2021-04-24 00:46:45 +0700

    config localhost port and remove unuse webpack config

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+// vue.config.js
+
+/**
+ * @type {import('@vue/cli-service')}
+ */
+
+module.exports = {
+    devServer: {
+        compress: true,
+        host: 'localhost',
+        port: 9000,
+    },
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,5 @@
-const { VueLoaderPlugin } = require('vue-loader')
+// webpack.config.js
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            // this will apply to both plain `.js` files
-            // AND `<script>` blocks in `.vue` files
-            {
-                test: /\.js$/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.scss$/,
-                exclude: 'node-modules',
-                use: [
-                    'vue-style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        // make sure to include the plugin for the magic
-        new VueLoaderPlugin()
-    ]
+    //...
 }
